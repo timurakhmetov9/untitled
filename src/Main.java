a518f863cf5bd22a395df3fc001b4839b64d83f0
timurakhmetov9 <timurakhmetov9@outlook.com>
2022-12-29 14:22:02 +0300

    выполнил правки

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-    }
-    public static void task1 (){
-        System.out.println("Решение");
+        System.out.println("Hello 1!");
     }
 }
