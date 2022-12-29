8c6112384632a582a4a88c8eb373defc9917d18c
timurakhmetov9 <timurakhmetov9@outlook.com>
2022-12-29 13:47:01 +0300

    выполнил первое дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+    }
+    public static void task1 (){
+        System.out.println("Решение");
     }
 }
